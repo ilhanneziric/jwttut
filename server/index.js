1a63a19462f279023309602ace2abed75199d8d1
ilhanneziric <ilhan.neziric@edu.fit.ba>
2022-04-02 00:36:27 +0200

    Revert "Deploy backend"
    This reverts commit af7f1b0015e28e1381c5cf2d1aa74d0cc58de22d.

--- server/index.js
+++ server/index.js
@@ -9,12 +9,6 @@ app.use('/auth', require('./routes/jwtAuth'));
 
 app.use('/dashboard', require('./routes/dashboard'));
 
-app.use('/', (req, res) => {
-    res.send('okej je');
-});
-
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => {
+app.listen(5000, () => {
     console.log('server is runing on port 5000');
 })
